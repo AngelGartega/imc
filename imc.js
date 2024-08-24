6efe8e80d4e87d1ffd1425590385ab3106ef8de6
AngelGartega <angelgarcia060602@gmail.com>
2024-08-24 01:19:26 -0600

    Primer commit

---
+++ imc.js
@@ -0,0 +1,17 @@
+
+let nombre = "Juan";
+let apellido = "Pérez";
+let edad = 30;
+let peso = 75;
+let estatura = 1.75;
+
+// Cálculo del IMC
+let imc = peso / (estatura * estatura);
+
+// Impresión de los datos
+console.log("Nombre: " + nombre);
+console.log("Apellido: " + apellido);
+console.log("Edad: " + edad + " años");
+console.log("Peso: " + peso + " kg");
+console.log("Estatura: " + estatura + " m");
+console.log("Índice de Masa Corporal (IMC): " + imc);
